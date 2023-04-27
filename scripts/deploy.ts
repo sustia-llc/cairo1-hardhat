6a785da7361a01a9e1921c7e1af385c337874dcd
Holly Grimm <holly.grimm@gmail.com>
2023-04-27 13:13:26 -0600

    :rotating_light: lint clean-up

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,7 +2,12 @@ import hardhat from "hardhat";
 import { getOZAccountForDeploy } from "../test/util";
 
 async function main() {
-    const account = await getOZAccountForDeploy();
+    let account = null;
+    if (hardhat.starknet.network === "alphaGoerli") {
+        return;
+    } else {
+        account = await getOZAccountForDeploy();
+    }
     const contractFactory = await hardhat.starknet.getContractFactory("cairo1");
     await account.declare(contractFactory, { maxFee: 1e18 });
     const contract = await account.deploy(contractFactory, { initial_balance: 0 });
